94b0cc8533284821a148861ccf194ec1f02f6f3b
Mike Olund <mike@oxd.com>
2024-06-10 06:47:43 -0700

    SCJ-211: Fix for failing migration on Openshift

--- database/Migrations/20240610044538_SetTimeZone.cs
+++ database/Migrations/20240610044538_SetTimeZone.cs
@@ -1,6 +1,6 @@
-using DotEnv.Core;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.Extensions.Configuration;
+using Npgsql;
 
 #nullable disable
 
@@ -12,24 +12,51 @@ namespace SCJ.Booking.Data.Migrations
         {
             if (migrationBuilder.ActiveProvider == "Npgsql.EntityFrameworkCore.PostgreSQL")
             {
-                new EnvLoader().Load();
-
-                IConfigurationBuilder builder = new ConfigurationBuilder()
-                    .SetBasePath(Directory.GetCurrentDirectory())
-                    .AddJsonFile("appsettings.json", true, true)
-                    .AddEnvironmentVariables();
-
-                IConfiguration configuration = builder.Build();
-
-                var connectionString = configuration["Data:DefaultConnection:ConnectionString"];
-                var connInfo = new Npgsql.NpgsqlConnectionStringBuilder(connectionString);
-
                 migrationBuilder.Sql(
-                    $"ALTER DATABASE {connInfo.Database} SET TIMEZONE TO 'America/Vancouver'"
+                    $"ALTER DATABASE {GetDatabaseName()} SET TIMEZONE TO 'America/Vancouver'"
                 );
             }
         }
 
-        protected override void Down(MigrationBuilder migrationBuilder) { }
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            if (migrationBuilder.ActiveProvider == "Npgsql.EntityFrameworkCore.PostgreSQL")
+            {
+                migrationBuilder.Sql(
+                    $"ALTER DATABASE {GetDatabaseName()} SET TIMEZONE TO 'Etc/UTC'"
+                );
+            }
+        }
+
+        /// <remarks>
+        ///    There is probably an easier way to alter the current database without hardcoding
+        ///    the database name, but parsing the name from the connection string seems to work
+        /// </remarks>
+        private static string GetDatabaseName()
+        {
+            var connectionString = GetConnectionString();
+            var connectionStringBuilder = new NpgsqlConnectionStringBuilder(connectionString);
+            return connectionStringBuilder.Database;
+        }
+
+        private static string GetConnectionString()
+        {
+            // the environment variable should always be present on our OpenShift deployments
+            string connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+
+            if (!string.IsNullOrEmpty(connectionString))
+            {
+                return connectionString;
+            }
+
+            // get it from the json config files if the environment variable was not found
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile("servicesettings.json", true, true)
+                .AddJsonFile("testsettings.json", true, true);
+            IConfiguration configuration = builder.Build();
+            return configuration["Data:DefaultConnection:ConnectionString"];
+        }
     }
 }
