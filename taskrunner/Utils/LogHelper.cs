c3bfd3a9e5fae4752aacda748bb7fcb1693254b9
Mike Olund <mike@oxd.com>
2024-06-27 16:12:41 -0700

    NOBUG: use appsettings to control taskrunner log levels

--- taskrunner/Utils/LogHelper.cs
+++ taskrunner/Utils/LogHelper.cs
@@ -1,6 +1,5 @@
 using Microsoft.Extensions.Configuration;
 using Serilog;
-using Serilog.Events;
 
 namespace SCJ.Booking.TaskRunner.Utils
 {
@@ -8,24 +7,10 @@ namespace SCJ.Booking.TaskRunner.Utils
     {
         public static ILogger GetLogger(IConfiguration configuration)
         {
-            // default log level is warning (less verbose)
-            var logLevel = LogEventLevel.Warning;
-
-            //check if this is running on a developer workstation (outside OpenShift)
-            string tagName = configuration["TAG_NAME"] ?? "";
-            if (tagName.ToLower().Equals("localdev"))
-            {
-                logLevel = LogEventLevel.Debug;
-            }
-
-            // check if this is the OpenShift development environment
-            if (tagName.ToLower().Equals("dev"))
-            {
-                logLevel = LogEventLevel.Information;
-            }
-
-            //setup error logger settings
-            return new LoggerConfiguration().WriteTo.Console(logLevel).CreateLogger();
+            return new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .WriteTo.Console()
+                .CreateLogger();
         }
     }
 }
