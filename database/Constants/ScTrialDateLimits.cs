20067bdec02e30c5f8deef30759655c9a3ce6d2c
Duncan MacKenzie <duncan@oxd.com>
2024-07-05 10:13:22 -0700

    SCJ-232: Add string variable

--- database/Constants/ScTrialDateLimits.cs
+++ database/Constants/ScTrialDateLimits.cs
@@ -4,5 +4,8 @@ namespace SCJ.Booking.Data.Constants
     {
         // Maximum number of dates that can be selected for the fair use lottery
         public const int MaxSelectionSize = 5;
+
+        // English string representation of MaxSelectionSize
+        public const string MaxSelectionSizeString = "five";
     }
 }
