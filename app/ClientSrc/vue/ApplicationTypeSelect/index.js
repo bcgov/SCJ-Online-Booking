152a9a309ffaec0f25035154a912397abeefa055
Mike Olund <mike@oxd.com>
2025-09-09 17:41:23 -0700

    SCJ-303: Bug fixes

--- app/ClientSrc/vue/ApplicationTypeSelect/index.js
+++ app/ClientSrc/vue/ApplicationTypeSelect/index.js
@@ -1,7 +1,14 @@
 import { createApp } from "vue";
 import ApplicationTypeSelect from "./ApplicationTypeSelect";
 
-const el = document.getElementById("VueApplicationTypeSelect");
-if (el) {
-  createApp(ApplicationTypeSelect).mount(el);
+// Mount vue app if a container element exists
+if (document.getElementById("VueApplicationTypeSelect")) {
+  const app = createApp({
+    methods: {
+      // bring functions (from /js/coa.js) into the vue app scope
+      onApplicationTypeChange,
+    },
+  });
+  app.component("ApplicationTypeSelect", ApplicationTypeSelect);
+  app.mount("#VueApplicationTypeSelect");
 }
