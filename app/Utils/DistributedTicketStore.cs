77f2dd83b6dac9b2501560061dc186409ceb0e86
Mike Olund <mike@oxd.com>
2025-11-03 14:30:57 -0800

    SCJ-000: Fix path

--- app/Utils/DistributedTicketStore.cs
+++ app/Utils/DistributedTicketStore.cs
@@ -7,112 +7,115 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.Extensions.Caching.Distributed;
 using Newtonsoft.Json;
 
-public class DistributedTicketStore : ITicketStore
+namespace SCJ.Booking.MVC.Utils
 {
-    private readonly IDistributedCache _distributedCache;
-    private readonly TimeSpan _expiration;
-
-    public DistributedTicketStore(IDistributedCache distributedCache, TimeSpan expiration)
+    public class DistributedTicketStore : ITicketStore
     {
-        _distributedCache = distributedCache;
-        _expiration = expiration;
-    }
+        private readonly IDistributedCache _distributedCache;
+        private readonly TimeSpan _expiration;
 
-    public async Task<string> StoreAsync(AuthenticationTicket ticket)
-    {
-        var key = Guid.NewGuid().ToString();
-        var ticketData = SerializeTicket(ticket);
-        var options = new DistributedCacheEntryOptions
+        public DistributedTicketStore(IDistributedCache distributedCache, TimeSpan expiration)
         {
-            AbsoluteExpirationRelativeToNow = _expiration
-        };
-        await _distributedCache.SetStringAsync(key, ticketData, options);
-        return key;
-    }
+            _distributedCache = distributedCache;
+            _expiration = expiration;
+        }
 
-    public async Task RenewAsync(string key, AuthenticationTicket ticket)
-    {
-        var ticketData = SerializeTicket(ticket);
-        var options = new DistributedCacheEntryOptions
+        public async Task<string> StoreAsync(AuthenticationTicket ticket)
         {
-            AbsoluteExpirationRelativeToNow = _expiration
-        };
-        await _distributedCache.SetStringAsync(key, ticketData, options);
-    }
+            var key = Guid.NewGuid().ToString();
+            var ticketData = SerializeTicket(ticket);
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = _expiration
+            };
+            await _distributedCache.SetStringAsync(key, ticketData, options);
+            return key;
+        }
+
+        public async Task RenewAsync(string key, AuthenticationTicket ticket)
+        {
+            var ticketData = SerializeTicket(ticket);
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = _expiration
+            };
+            await _distributedCache.SetStringAsync(key, ticketData, options);
+        }
 
 #pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
-    public async Task<AuthenticationTicket?> RetrieveAsync(string key)
+        public async Task<AuthenticationTicket?> RetrieveAsync(string key)
 #pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
-    {
-        var ticketData = await _distributedCache.GetStringAsync(key);
-        if (ticketData != null)
         {
-            return DeserializeTicket(ticketData);
+            var ticketData = await _distributedCache.GetStringAsync(key);
+            if (ticketData != null)
+            {
+                return DeserializeTicket(ticketData);
+            }
+            return null;
         }
-        return null;
-    }
 
-    public Task RemoveAsync(string key)
-    {
-        return _distributedCache.RemoveAsync(key);
-    }
-
-    private string SerializeTicket(AuthenticationTicket ticket)
-    {
-        var ticketModel = new TicketModel
+        public Task RemoveAsync(string key)
         {
-            AuthenticationScheme = ticket.AuthenticationScheme,
-            Properties = ticket.Properties,
-            Claims = ticket
-                .Principal.Claims.Select(c => new ClaimModel
-                {
-                    Type = c.Type,
-                    Value = c.Value,
-                    Issuer = c.Issuer,
-                    OriginalIssuer = c.OriginalIssuer
-                })
-                .ToList()
-        };
+            return _distributedCache.RemoveAsync(key);
+        }
 
-        return JsonConvert.SerializeObject(ticketModel);
-    }
+        private string SerializeTicket(AuthenticationTicket ticket)
+        {
+            var ticketModel = new TicketModel
+            {
+                AuthenticationScheme = ticket.AuthenticationScheme,
+                Properties = ticket.Properties,
+                Claims = ticket
+                    .Principal.Claims.Select(c => new ClaimModel
+                    {
+                        Type = c.Type,
+                        Value = c.Value,
+                        Issuer = c.Issuer,
+                        OriginalIssuer = c.OriginalIssuer
+                    })
+                    .ToList()
+            };
 
-    private AuthenticationTicket DeserializeTicket(string serializedTicket)
-    {
-        var ticketModel = JsonConvert.DeserializeObject<TicketModel>(serializedTicket);
+            return JsonConvert.SerializeObject(ticketModel);
+        }
 
-        var claims = ticketModel.Claims.Select(c => new System.Security.Claims.Claim(
-            c.Type,
-            c.Value,
-            null,
-            c.Issuer,
-            c.OriginalIssuer
-        ));
-        var identity = new System.Security.Claims.ClaimsIdentity(
-            claims,
-            ticketModel.AuthenticationScheme
-        );
-        var principal = new System.Security.Claims.ClaimsPrincipal(identity);
+        private AuthenticationTicket DeserializeTicket(string serializedTicket)
+        {
+            var ticketModel = JsonConvert.DeserializeObject<TicketModel>(serializedTicket);
 
-        return new AuthenticationTicket(
-            principal,
-            ticketModel.Properties,
-            ticketModel.AuthenticationScheme
-        );
-    }
+            var claims = ticketModel.Claims.Select(c => new System.Security.Claims.Claim(
+                c.Type,
+                c.Value,
+                null,
+                c.Issuer,
+                c.OriginalIssuer
+            ));
+            var identity = new System.Security.Claims.ClaimsIdentity(
+                claims,
+                ticketModel.AuthenticationScheme
+            );
+            var principal = new System.Security.Claims.ClaimsPrincipal(identity);
 
-    private class TicketModel
-    {
-        public string AuthenticationScheme { get; set; }
-        public AuthenticationProperties Properties { get; set; }
-        public List<ClaimModel> Claims { get; set; }
-    }
+            return new AuthenticationTicket(
+                principal,
+                ticketModel.Properties,
+                ticketModel.AuthenticationScheme
+            );
+        }
 
-    private class ClaimModel
-    {
-        public string Type { get; set; }
-        public string Value { get; set; }
-        public string Issuer { get; set; }
-        public string OriginalIssuer { get; set; }
+        private class TicketModel
+        {
+            public string AuthenticationScheme { get; set; }
+            public AuthenticationProperties Properties { get; set; }
+            public List<ClaimModel> Claims { get; set; }
+        }
+
+        private class ClaimModel
+        {
+            public string Type { get; set; }
+            public string Value { get; set; }
+            public string Issuer { get; set; }
+            public string OriginalIssuer { get; set; }
+        }
     }
 }
